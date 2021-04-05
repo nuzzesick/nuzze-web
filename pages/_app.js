67a38638d1de98424cfcd7236f8ea37fa03de23e
nuzzesick <mati280341@gmail.com>
2021-04-05 16:50:02 -0300

    added tailwind and linter && fixed linter issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component pageProps={pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
+
+export default MyApp;
