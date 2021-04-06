2299c4b7460aeebb6f9d6246983f69d3028b6c81
nuzzesick <mati280341@gmail.com>
2021-04-06 04:37:12 -0300

    customizing colors on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          600: '#575757',
+          800: '#262626',
+          900: '#131313',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
