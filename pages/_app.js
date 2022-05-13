1d531403fc2db7aac3c08ac3173443a9452ad9c4
nuzzesick <mati280341@gmail.com>
2022-05-13 14:21:08 -0300

    updated metadata

--- pages/_app.js
+++ pages/_app.js
@@ -12,13 +12,13 @@ const App = ({ Component, pageProps }) => {
         <meta charset="utf-8" />
         <title>Nuzze</title>
         <meta name="viewport" content="width=device-width" />
-        <meta name="description" content="I'm Nuzze a 21 years old Software developer born in La Plata, Buenos Aires." />
+        <meta name="description" content="Software developer born and raised in Buenos Aires." />
         <meta property="og:title" content="Nuzze" />
         <meta property="og:site_name" content="Nuzze" />
         <meta name="keywords" content="Software, Developer, Programmer, Web, Fullstack, Frontend, Backend" />
         <meta name="author" content="Nuzze" />
         <meta property="og:url" content="https://www.nuzze.co" />
-        <meta property="og:description" content="I'm Nuzze a 21 years old Software developer born in La Plata, Buenos Aires." />
+        <meta property="og:description" content="Software developer born and raised in Buenos Aires." />
         <meta property="og:image" content="https://www.nuzze.co/images/seo-image.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" value="@nuzzze" />
